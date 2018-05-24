2615c78e4d6c48ee50d998692c7229cf97504cfe
shreyansh <shreyansh.barodiya@gmail.com>
2018-05-24 18:12:27 +0530

    analysis

--- urls.py
+++ urls.py
@@ -4,5 +4,5 @@ import views
 urlpatterns = patterns(
     '',
     url(r'^hello$', views.hello, name = 'hello'),
-    url(r'^question_stats/<xblock_id>$', views.question_stats, name='question_stats'),
+    url(r'^question_stats/(?P<xblock_id>[A-Za-z0-9_.-]+)/$', views.question_stats, name='question_stats'),
 )
